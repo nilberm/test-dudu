d00f162f4ccd0087c43636b3868ced2835f2646b
nilberm <nilber16@outlook.com>
2024-02-06 14:43:59 -0300

    submit test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Container from "@/components/Containers";
 import styles from "./page.module.css";
-import { GridContainers } from "./style";
+import { ContainerContent, GridContainers } from "./style";
+import { useState } from "react";
 
 export default function Home() {
   const tempContentArray = [
@@ -17,14 +17,106 @@ export default function Home() {
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat libero sunt voluptates sint nesciunt eligendi architecto amet quibusdam ea, alias ducimus atque obcaecati beatae aliquam, a asperiores laudantium accusamus iste.",
   ];
 
+  const [scenarioSelected, setScenarioSelected] = useState<string>("0");
+
   return (
     <main className={styles.main}>
-      <GridContainers>
+      <select
+        name="optionsGrid"
+        id="optionsGrid"
+        onChange={(v) => setScenarioSelected(v.target.value)}
+      >
+        <option value={0}>default</option>
+        <option value={1}>scenario 1</option>
+        <option value={2}>scenario 2</option>
+        <option value={3}>scenario 3</option>
+        <option value={4}>scenario 4</option>
+      </select>
+
+      <GridContainers scenarioSelected={scenarioSelected}>
         {tempContentArray.map((item: string, index) => {
+          if (scenarioSelected === "1") {
+            if (index === 0) {
+              return (
+                <ContainerContent key={index} className={`container-${index}`}>
+                  {tempContentArray[0]} {tempContentArray[1]}
+                </ContainerContent>
+              );
+            }
+            if (index === 1) {
+              return;
+            }
+
+            return (
+              <ContainerContent key={index} className={`container-${index}`}>
+                {item}
+              </ContainerContent>
+            );
+          }
+
+          if (scenarioSelected === "2") {
+            if (index === 0) {
+              return (
+                <ContainerContent key={index} className={`container-${index}`}>
+                  {tempContentArray[0]} {tempContentArray[1]}{" "}
+                  {tempContentArray[1]}
+                </ContainerContent>
+              );
+            }
+            if (index === 1 || index === 2) {
+              return;
+            }
+
+            return (
+              <ContainerContent key={index} className={`container-${index}`}>
+                {item}
+              </ContainerContent>
+            );
+          }
+
+          if (scenarioSelected === "3") {
+            if (index === 0) {
+              return (
+                <ContainerContent key={index} className={`container-${index}`}>
+                  {tempContentArray[0]} {tempContentArray[3]}{" "}
+                </ContainerContent>
+              );
+            }
+            if (index === 3) {
+              return;
+            }
+
+            return (
+              <ContainerContent key={index} className={`container-${index}`}>
+                {item}
+              </ContainerContent>
+            );
+          }
+
+          if (scenarioSelected === "4") {
+            if (index === 0) {
+              return (
+                <ContainerContent key={index} className={`container-${index}`}>
+                  {tempContentArray[0]} {tempContentArray[3]}{" "}
+                  {tempContentArray[6]}
+                </ContainerContent>
+              );
+            }
+            if (index === 3 || index === 6) {
+              return;
+            }
+
+            return (
+              <ContainerContent key={index} className={`container-${index}`}>
+                {item}
+              </ContainerContent>
+            );
+          }
+
           return (
-            <Container key={index} position={index}>
+            <ContainerContent key={index} className={`container-${index}`}>
               {item}
-            </Container>
+            </ContainerContent>
           );
         })}
       </GridContainers>
--- src/app/style.ts
+++ src/app/style.ts
@@ -1,9 +1,94 @@
 import styled from "styled-components";
 
-export const GridContainers = styled.div`
+interface gridContainerProps {
+  scenarioSelected: string;
+}
+
+export const GridContainers = styled.div<gridContainerProps>`
   display: grid;
-  grid-gap: 2rem;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  position: relative;
+  grid-gap: 1rem;
+
+  .container-0 {
+    grid-area: a0;
+  }
+
+  .container-1 {
+    grid-area: a1;
+  }
+  .container-2 {
+    grid-area: a2;
+  }
+  .container-3 {
+    grid-area: a3;
+  }
+  .container-4 {
+    grid-area: a4;
+  }
+  .container-5 {
+    grid-area: a5;
+  }
+  .container-6 {
+    grid-area: a6;
+  }
+  .container-7 {
+    grid-area: a7;
+  }
+  .container-8 {
+    grid-area: a8;
+  }
+
+  /* Ajuste do grid com base na seleção */
+
+  ${(props) =>
+    props.scenarioSelected === "0" &&
+    `
+    grid-template-areas:
+    "a0 a1 a2"
+    "a3 a4 a5"
+    "a6 a7 a8";
+  `}
+
+  ${(props) =>
+    props.scenarioSelected === "1" &&
+    `
+    grid-template-areas:
+    "a0 a0 a2"
+    "a3 a4 a5"
+    "a6 a7 a8";
+
+  `} 
+
+${(props) =>
+    props.scenarioSelected === "2" &&
+    `
+    grid-template-areas:
+    "a0 a0 a0"
+    "a3 a4 a5"
+    "a6 a7 a8";
+
+  `}
+
+${(props) =>
+    props.scenarioSelected === "3" &&
+    `
+    grid-template-areas:
+    "a0 a1 a2"
+    "a0 a4 a5"
+    "a6 a7 a8";
+
+  `}
+
+${(props) =>
+    props.scenarioSelected === "4" &&
+    `
+    grid-template-areas:
+    "a0 a1 a2"
+    "a0 a4 a5"
+    "a0 a7 a8";
+
+  `}
+`;
+
+export const ContainerContent = styled.div`
+  display: grid;
 `;
